f6be5327d045058ccfcb053d57216d9c1f58ea3f
Sergey Onoyko <sergey.v.onoyko@gmail.com>
2021-02-18 12:34:38 +0100

    associations are in place

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,6 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail, required: true
   belongs_to :ingredient, required: true
 
-  validates :description, presence: true
-  validates_uniqueness_of :cocktail_id, :scope => { :ingredient_id }
+  validates :description, presence: true, allow_blank: false
+  validates_uniqueness_of :cocktail_id, :scope => :ingredient_id
 end
