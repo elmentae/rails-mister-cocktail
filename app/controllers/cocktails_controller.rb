93920b573765f093e1376ac2d9752db8e6e54431
Sergey Onoyko <sergey.v.onoyko@gmail.com>
2021-02-19 16:05:11 +0100

    logic between user picture upload and user picture url upload added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,6 @@
 class CocktailsController < ApplicationController
+  before_action :default_image
+
   def index
     @cocktail = Cocktail.all
   end
@@ -27,6 +29,10 @@ class CocktailsController < ApplicationController
   end
 
   private
+
+  def default_image
+    @def_img = "https://i.ytimg.com/vi/zAVGfPSnGaw/maxresdefault.jpg"
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name, :image_url, :description, :photo)
